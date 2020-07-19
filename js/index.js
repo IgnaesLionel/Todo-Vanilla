986bd2d8e63b6c14c773fc809c429366c041998f
ignaes_Lionel@hotmail.com <ignaes_lionel@hotmail.com>
2020-07-19 10:30:14 +0200

    todo

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const list = document.getElementById('list')
+const input = document.getElementById('input')
+const add = document.getElementById('add')
+const clear = document.getElementById('clear')
+const url = document.getElementById('url')
+const load = document.getElementById('load')
+
+const tasks = ['Faire la vaisselle', 'Ranger le salon']
+
+console.log(url)
+
+const num1 = {
+  clef1 : "lol",
+  clef2 : "lolilol"
+}
+
+console.log(num1)
+
+num1[clef1]="test"
+
+console.log(num1)
