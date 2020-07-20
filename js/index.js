9b3f0b7ebb6db1c0fd71275c168582a04cffb94b
ignaes_Lionel@hotmail.com <ignaes_lionel@hotmail.com>
2020-07-20 17:52:18 +0200

    ajout boutton

--- js/index.js
+++ js/index.js
@@ -4,5 +4,20 @@ const add = document.getElementById('add')
 const clear = document.getElementById('clear')
 const url = document.getElementById('url')
 const load = document.getElementById('load')
-
 const tasks = ['Faire la vaisselle', 'Ranger le salon']
+
+for (let i = 0; i < tasks.length; i++){
+  //vérification du type string et chaine non-vide
+  if (typeof tasks[i] === 'string' && tasks[i]){
+    //creation balise li
+    const li = document.createElement('li')
+    //creation d'un boutton
+    const removebtn = document.createElement('button')
+
+    li.textContent= tasks[i]
+    removebtn.textContent= 'Remove'
+    li.appendChild(removebtn)
+    // injection
+    list.insertBefore(li, list.firstChild)
+  }
+}
